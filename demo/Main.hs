a35f629b4be20473fe877a2e7c2e1b34be0ca56b
Ivan Gromakovskii <gie1994@gmail.com>
2020-08-10 20:45:25 +0300

    Add demo
    Problem: we want to demonstrate that the library functions work.
    Solution: add an executable that uses one of the functions depending
    on the command line argument.

---
+++ demo/Main.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Main
+  ( main
+  ) where
+
+import Control.Exception (Exception(..), throwIO)
+import System.Environment (getArgs)
+
+import Control.Exception.Uncaught
+
+data MyException = MyException
+  deriving stock Show
+
+instance Exception MyException where
+  displayException MyException = "displayException"
+
+main :: IO ()
+main = do
+  method <- getArgs >>= \case
+    [m] -> pure m
+    _ -> fail "Pass only one argument"
+  f method $ throwIO MyException
+  where
+    f :: String -> IO a -> IO a
+    f "id" = id
+    f "displayUncaughtException" = displayUncaughtException
+    f "withDisplayExceptionHandler" = withDisplayExceptionHandler
+    f "setDisplayExceptionHandler" = (setDisplayExceptionHandler >>)
+    f method = const $ fail $ "Unknown method: " <> method

bb98979a05bcc879b745d4f6a4c345e384cdcea9
Ivan Gromakovskii <gie1994@gmail.com>
2020-08-11 17:31:11 +0300

    Add reuse check to CI and make it pass
    Problem: we want all our repositories to have machine-readable
    copyright information and we normally use the `reuse` tool to ensure
    that. It's not used here yet.
    Solution: add such a check to CI, add missing licenses and copyright
    information.

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #/usr/bin/env bash
 
+# SPDX-FileCopyrightText: 2020 Serokell <https://serokell.io>
+#
+# SPDX-License-Identifier: MPL-2.0
+
 bin/uncaught-exception-demo id 2>&1 >/dev/null | grep MyException
 bin/uncaught-exception-demo displayUncaughtException 2>&1 > /dev/null | grep displayException
 bin/uncaught-exception-demo withDisplayExceptionHandler 2>&1 > /dev/null | grep displayException
